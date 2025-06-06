0480495abc730c85ab1abad64822a7a94d1841e0
Mirshod Mirjonov <mirshod.mirjonov@tangoanalytics.com>
2025-06-06 10:53:43 +0500

    more tree

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -9,8 +9,44 @@ LNR
 
 
 def inorder_traversal(root: BinaryTreeNode) -> List[int]:
+    res = []
+    node = root
+    DOWN, UP = 0, 1
+    DIRECTION = DOWN
 
-    return []
+    while node:
+        if DIRECTION == DOWN:
+            node = left_most_child(node)
+            DIRECTION, node = process_node(DIRECTION, UP, DOWN, node, res)
+        else:
+            node = lowest_left_wise_parent(node)
+            if node is None:
+                break
+            DIRECTION, node = process_node(DIRECTION, UP, DOWN, node, res)
+
+    return res
+
+
+def process_node(DIRECTION, UP, DOWN, node, res):
+    res.append(node.val)
+    if node.right is not None:
+        node = node.right
+        DIRECTION = DOWN
+    else:
+        DIRECTION = UP
+    return DIRECTION, node
+
+
+def left_most_child(node):
+    while node.left is not None:
+        node = node.left
+    return node
+
+
+def lowest_left_wise_parent(node):
+    while node.parent and node.parent.right == node:
+        node = node.parent
+    return node.parent
 
 
 if __name__ == '__main__':
